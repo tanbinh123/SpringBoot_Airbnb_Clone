2890cd63a85aaae8c256b46b3d4416767789f126
ro-el-c <chung3286@naver.com>
2022-06-25 16:52:01 +0900

    Feat: 숙소 상세 페이지 model 생성 및 controller 수정

---
+++ server/airbnb_server/src/main/java/com/example/demo/src/accommodation/model/GetAccomDetailReq.java
@@ -0,0 +1,13 @@
+package com.example.demo.src.accommodation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetAccomDetailReq {
+    private int accom_id;
+
+}
