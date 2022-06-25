fffc91740551d5ac75c30a8533304b748b5aff13
ro-el-c <chung3286@naver.com>
2022-06-25 11:04:04 +0900

    Spring project 생성

---
+++ server/udemy_server_practice_springboot/src/main/java/com/example/demo/src/post/PostController.java
@@ -0,0 +1,115 @@
+package com.example.demo.src.post;
+
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.config.BaseResponseStatus;
+import com.example.demo.src.post.model.GetPostsRes;
+import com.example.demo.src.post.model.PatchPostsReq;
+import com.example.demo.src.post.model.PostPostsReq;
+import com.example.demo.src.post.model.PostPostsRes;
+import com.example.demo.src.user.UserProvider;
+import com.example.demo.src.user.UserService;
+import com.example.demo.src.user.model.*;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.example.demo.config.BaseResponseStatus.*;
+import static com.example.demo.utils.ValidationRegex.isRegexEmail;
+
+@RestController
+@RequestMapping("/posts") // controller 에 있는 모든 api 의 uri 앞에 기본적으로 들어감
+public class PostController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final PostProvider postProvider;
+    @Autowired
+    private final PostService postService;
+    @Autowired
+    private final JwtService jwtService;
+
+
+    public PostController(PostProvider postProvider, PostService postService, JwtService jwtService){
+        this.postProvider = postProvider;
+        this.postService = postService;
+        this.jwtService = jwtService;
+    }
+
+    @ResponseBody
+    @GetMapping("")
+    public BaseResponse<List<GetPostsRes>> getPosts(@RequestParam(required = true)  int userIdx) {
+        try{
+            List<GetPostsRes> getPostsRes = postProvider.retrievePosts(userIdx);
+            return new BaseResponse<>(getPostsRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    @ResponseBody
+    @PostMapping("")
+    public BaseResponse<PostPostsRes> createPost(@RequestBody PostPostsReq postPostsReq) {
+
+        try{
+            int userIdxByJwt = jwtService.getUserIdx();
+            if(postPostsReq.getUserIdx() != userIdxByJwt)
+                return new BaseResponse<>(INVALID_USER_JWT);
+
+            // 이미지, 게시글에 대한 validation
+            // 게시글의 길이가 제한 글자수보다 길 때
+            if(postPostsReq.getContent().length() > 450)
+                return new BaseResponse<>(BaseResponseStatus.POST_POSTS_INVALID_CONTENTS);
+            // 이미지가 없을 때
+            if(postPostsReq.getPostImgUrls().size() < 1)
+                return new BaseResponse<>(BaseResponseStatus.POST_POSTS_EMPTY_IMGURL);
+
+            PostPostsRes getPostsRes = postService.createPost(userIdxByJwt, postPostsReq);
+
+            // 무언가를 만드는 것이기 때문에 Service로 처리
+            return new BaseResponse<>(getPostsRes);
+        } catch(BaseException exception){
+            exception.printStackTrace();
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    @ResponseBody
+    @PatchMapping("/{postIdx}")
+    public BaseResponse<String> modifyPosts(@PathVariable("postIdx") int postIdx, @RequestBody PatchPostsReq patchPostsReq) {
+        try{
+            // 게시글에 대한 validation - 게시물 수정 시 이미지 변경 불가, 게시글만 변경 가능
+            // 게시글의 길이가 제한 글자수보다 길 때
+            if(patchPostsReq.getContent().length() > 450)
+                return new BaseResponse<>(BaseResponseStatus.POST_POSTS_INVALID_CONTENTS);
+
+            String result = "게시물 수정을 완료하였습니다.";
+
+            postService.modifyPost(patchPostsReq.getUserIdx(), postIdx, patchPostsReq);
+
+            return new BaseResponse<>(result);
+        } catch(BaseException exception){
+            exception.printStackTrace();
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    @ResponseBody
+    @PatchMapping("/{postIdx}/status")
+    public BaseResponse<String> deletePosts(@PathVariable("postIdx") int postIdx) {
+        try{
+            String result = "게시물 삭제를 완료하였습니다.";
+
+            postService.deletePost(postIdx);
+
+            return new BaseResponse<>(result);
+        } catch(BaseException exception){
+            exception.printStackTrace();
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+}
